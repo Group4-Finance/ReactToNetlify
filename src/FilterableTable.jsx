961183380a65e18b7fe209be4386654e137c593f
kong567 <david.5516kimo@gmail.com>
2025-08-08 01:29:07 +0800

    1

--- src/FilterableTable.jsx
+++ src/FilterableTable.jsx
@@ -73,8 +73,8 @@ function DatabaseQuery({ instanceName }) {
     setError(null);
 
     // 組 API URL，假設格式為 http://ip:port/資料表名稱
-    let url = `${process.env.REACT_APP_etfgoup4}/${queryParams.table}`;
-    // let url = `https://35.206.205.183:8888/${queryParams.table}`;
+    // let url = `${process.env.REACT_APP_etfgoup4}/${queryParams.table}`;
+    let url = `https://35.206.205.183:8888/${queryParams.table}`;
 
     // 組查詢參數字串
     const params = [];
